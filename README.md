b47444c1fdf2e470ec76eb13046ac72f80156ee3
Chris Boon <cboon27@gmail.com>
2018-03-28 13:18:51 -0400

    Add project t Git

---
+++ README.md
@@ -0,0 +1,5 @@
+# OUP-oxford-learn-demo
+
+The site was created to showcase the basic functionality and UI of an upcoming platform we were to launch in 2012/13. It was aimed at sales reps, so they could get an expectation of what was coming and show to key customers.
+
+This was a fairly simple static site, but it did use web manifest to allow for offline usage - which was way harder in 2012 than it is now. Offline was key as we wanted reps to be able to demo it without an internet connection, yet we didn't want them to have to download the site and run locally.
---
+++ js/facs-hilite.js
@@ -0,0 +1,101 @@
+			$(document).ready(function() {
+			
+			$(".hint1").click(function () {
+				 if ($(this).hasClass("thisishilited")) {
+            $(this).removeClass("thisishilited");
+            $("span").removeClass("hilited");
+        } else{
+					$(".hint").removeClass("thisishilited");
+					$("span").removeClass("hilited");
+					$(this).addClass("thisishilited");
+					$(".hilite1").addClass("hilited");}
+			});
+
+			$(".hint2").click(function () {
+				 if ($(this).hasClass("thisishilited")) {
+            $(this).removeClass("thisishilited");
+            $("span").removeClass("hilited");
+        } else{
+					$(".hint").removeClass("thisishilited");
+					$("span").removeClass("hilited");
+					$(this).addClass("thisishilited");
+					$(".hilite2").addClass("hilited");}
+			});
+			$(".hint3").click(function () {
+				 if ($(this).hasClass("thisishilited")) {
+            $(this).removeClass("thisishilited");
+            $("span").removeClass("hilited");
+        } else{
+					$(".hint").removeClass("thisishilited");
+					$("span").removeClass("hilited");
+					$(this).addClass("thisishilited");
+					$(".hilite3").addClass("hilited");}
+			});
+			
+			$(".hint4").click(function () {
+				 if ($(this).hasClass("thisishilited")) {
+            $(this).removeClass("thisishilited");
+            $("span").removeClass("hilited");
+        } else{
+					$(".hint").removeClass("thisishilited");
+					$("span").removeClass("hilited");
+					$(this).addClass("thisishilited");
+					$(".hilite4").addClass("hilited");}
+			});
+
+			$(".hint5").click(function () {
+				 if ($(this).hasClass("thisishilited")) {
+            $(this).removeClass("thisishilited");
+            $("span").removeClass("hilited");
+        } else{
+					$(".hint").removeClass("thisishilited");
+					$("span").removeClass("hilited");
+					$(this).addClass("thisishilited");
+					$(".hilite5").addClass("hilited");}
+			});
+			$(".hint6").click(function () {
+				 if ($(this).hasClass("thisishilited")) {
+            $(this).removeClass("thisishilited");
+            $("span").removeClass("hilited");
+        } else{
+					$(".hint").removeClass("thisishilited");
+					$("span").removeClass("hilited");
+					$(this).addClass("thisishilited");
+					$(".hilite6").addClass("hilited");}
+			});
+			
+			$(".hint7").click(function () {
+				 if ($(this).hasClass("thisishilited")) {
+            $(this).removeClass("thisishilited");
+            $("span").removeClass("hilited");
+        } else{
+					$(".hint").removeClass("thisishilited");
+					$("span").removeClass("hilited");
+					$(this).addClass("thisishilited");
+					$(".hilite7").addClass("hilited");}
+			});
+
+			$(".hint8").click(function () {
+				 if ($(this).hasClass("thisishilited")) {
+            $(this).removeClass("thisishilited");
+            $("span").removeClass("hilited");
+        } else{
+					$(".hint").removeClass("thisishilited");
+					$("span").removeClass("hilited");
+					$(this).addClass("thisishilited");
+					$(".hilite8").addClass("hilited");}
+			});
+			$(".hint9").click(function () {
+				 if ($(this).hasClass("thisishilited")) {
+            $(this).removeClass("thisishilited");
+            $("span").removeClass("hilited");
+        } else{
+					$(".hint").removeClass("thisishilited");
+					$("span").removeClass("hilited");
+					$(this).addClass("thisishilited");
+					$(".hilite9").addClass("hilited");}
+			});
+			
+			
+				
+			});				
